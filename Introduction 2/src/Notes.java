80a2eb8ff2749a05722b54787a3fc3781c3d499b
williampho <williampho@10.0.1.24>
2020-10-14 20:58:49 -0400

    Push (2nd attempt)

---
+++ Introduction 2/src/Notes.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/*
+* Author : williampho
+* Date : 14-Oct-2020
+*/
+public class Notes
+	{
+
+		public static void main(String[] args)
+			{
+					{
+						int maNote;
+						Scanner sc=new Scanner(System.in);
+						System.out.println("Quelle est ta note?");
+						maNote=sc.nextInt();
+						
+						if(maNote>=7)
+							System.out.println("Exellente 97-100%");
+						else
+							if(maNote>=6)
+								System.out.println("Tres Bien 93-96%");
+							else
+								if(maNote>=5)
+									System.out.println("Bien 85-92%");
+								else
+									if(maNote>=4)
+										System.out.println("Satisfaisant 74-84%");
+									else
+										if(maNote>=3)
+											System.out.println("Mediocre 63-73%");
+										else
+											if(maNote>=2)
+												System.out.println("Poor 53-62%");
+											else
+												if(maNote>=1)
+													System.out.println("FAIL! <50%");
+
+					}
+
+			}
+
+
+			}
+
