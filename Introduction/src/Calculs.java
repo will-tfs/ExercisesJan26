184b66a00c9999df02c0ce56dc8d4b5abf4ffb85
williampho <williampho@localhost>
2020-10-19 10:15:40 -0400

    push

--- Introduction/src/Calculs.java
+++ Introduction/src/Calculs.java
@@ -10,7 +10,7 @@ public class Calculs
 		public static void main(String[] args)
 			{
 				Scanner sc=new Scanner(System.in);
-				int a,b=0
+				int a,b=0;
 				int resultat=0;
 				a=sc.nextInt();
 				b=sc.nextInt();
