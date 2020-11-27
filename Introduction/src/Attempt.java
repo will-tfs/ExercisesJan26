876665bd81e676ecb01952eeef80c909f6a7b4ea
williampho <williampho@localhost>
2020-11-27 14:43:56 -0500

    Mon Traville du 27 Novembre

---
+++ Introduction/src/Attempt.java
@@ -0,0 +1,23 @@
+/*
+* Author : williampho
+* Date : 27-Nov-2020
+*/
+public class Attempt
+	{
+
+		public static void main(String[] args)
+			{
+			    
+		        int [] arr = new int [] {4883772, 578728837, 8727161, 8827277, 9972722, 7722929, 12829393, 9873648}; 
+		        int min = arr[0]; 
+		        for (int i = 0; i < arr.length; i++) {    
+		           if(arr[i] <min)  
+		               min = arr[i];  
+		        }  
+		        System.out.println("Valeur le plus petite: " + min);  
+		    }  
+			{  
+
+			}
+
+	}
